7baa36056b85f21ede3bb66f574306695e243148
JenBrainnet <benebrak01@gmail.com>
2026-03-23 02:18:20 +0200

    add title

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,8 +2,10 @@ package ru.job4j;
 
 public class Main {
 
+    private String title = "Pack Man";
+
     public static void main(String[] args) {
-        System.out.println("Hello, Git!");
+        System.out.println("Hello, World!");
     }
 
 }
